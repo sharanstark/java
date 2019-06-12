a790373b84a1b39c3bb01cafd27fa4ecee436561
Sharan Stark <42034566+sharanstark@users.noreply.github.com>
2019-06-12 18:04:13 +0530

    Update BracketsChecker.java

--- BracketsChecker.java
+++ BracketsChecker.java
@@ -11,29 +11,29 @@ public class BracketsChecker {
 		System.out.println("\n...working...\n");
 		String o="([{";
 		String c=")]}";
-		String x="";
-		int check=0;
+		String x=" ";
+		int check =0;
 		for (int i = 0; i < s.length(); i++) {
-			if(o.contains(String.valueOf(s.charAt(i))) && check==0){
-				x+=s.charAt(i);
-				 System.out.println("In : "+x); // stack in
-			}else if(c.contains(String.valueOf(s.charAt(i))) && check==0) {
-				try {
-				x=x.substring(0,x.length()-1);
-				System.out.println("Out : "+x);   // stack out
-				}
-				catch(StringIndexOutOfBoundsException e) {
-					System.out.println("\n\nCompilation Error : opening bracket(s) missing \n\n© github.com/sharanstark 2k19" );
-					check=1;
-				}
+			if(o.contains(String.valueOf(s.charAt(i)))){
+				x+=s.charAt(i);		
+				 //System.out.println("In : "+x); // stack in
+			}else if(c.contains(String.valueOf(s.charAt(i)))) {	
+				char temp = s.charAt(i);
+				if(temp==')') temp = '(';
+				if(temp=='}') temp = '{';
+				if(temp==']') temp = '[';
+				if(x.charAt(x.length()-1)==temp) {
+					x=" "+x.substring(1,x.length()-1);
+					//System.out.println("Out : "+x);   // stack out
+			}else {
+			check=1;	
+			}
 			}
 	}	
-		if(check==0) {
-		if(x.length()==0 ) {
+		if(x.length()==1 && check==0 ) {
 			System.out.println("\n\nCompilation Success \n\n© github.com/sharanstark 2k19");
 		}else {
-			System.out.println("\n\nCompilation Error : closing bracket(s) missing \n\n© github.com/sharanstark 2k19" );
+			System.out.println("\n\nCompilation Error \n\n© github.com/sharanstark 2k19" );
 		}
-	  }
-	} 
+	}
 }
